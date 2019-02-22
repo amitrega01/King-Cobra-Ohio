f46be3b99f82bed2996c0d124e2a26023d73e811
Adam Mitręga <amitrega01@gmail.com>
2019-02-22 12:01:09 +0100

    Update App.js
    added redux to project

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
+import React, { Component } from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './src/reducer';
+
 const MainNavigator = createStackNavigator({
   Home: { screen: HomeScreen }
 });
-
-const App = createAppContainer(MainNavigator);
-
+const AppContainer = createAppContainer(MainNavigator);
+const store = createStore(reducer);
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
+  }
+}
 export default App;
