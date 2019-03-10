625e0c625b96bcd5a853f8a8c7676736110f19a1
Adam <amitrega01@gmail.com>
2019-03-10 13:08:54 +0100

    added settings btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import Strings from '../consts/Strings';
 import Colors from '../consts/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class Header extends Component {
   render() {
     return (
       <View style={styles.wrapper}>
-        <Text>{Strings.appTitle} </Text>
+        <Text style={styles.appTitleText}>{Strings.appTitle}</Text>
+        <TouchableOpacity onPress={this.props.onPress}>
+          <Ionicons name="md-settings" size={32} color="white" />
+        </TouchableOpacity>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   wrapper: {
-    background: Colors.mainColor,
-    padding: 16
+    backgroundColor: Colors.mainColor,
+    padding: 16,
+    paddingTop: 44,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center'
+  },
+  appTitleText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#fff',
+    letterSpacing: 5
   }
 });
