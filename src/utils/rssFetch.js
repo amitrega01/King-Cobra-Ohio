9e8f950cf895d46f6b58bca1f1f7da0178981e65
Adam <amitrega01@gmail.com>
2019-03-11 20:19:31 +0100

    made rssfetch a function

--- src/utils/rssFetch.js
+++ src/utils/rssFetch.js
@@ -1,25 +1,22 @@
 const rp = require('request-promise-native');
 const $ = require('cheerio');
-const url = "https://www.rmf.fm/rss/"
 
-rp(url)
-  .then(function(html){
-    const urls = [];
-    for (let i = 0; i < $('div a', html).length; i++) {
-     var temp =$('div a', html)[i].attribs.href;
-     urls.push(temp+"");
-     
- 
-    }
-    let rss = [];
-    urls.forEach((item)=> {
-      if(item.includes("feed") ||item.includes("rss") ) rss.push(item)
+export default function rssFetch(url) {
+  rp(url)
+    .then(function(html) {
+      const urls = [];
+      for (let i = 0; i < $('div a', html).length; i++) {
+        var temp = $('div a', html)[i].attribs.href;
+        urls.push(temp + '');
+      }
+      let rss = [];
+      urls.forEach(item => {
+        if (item.includes('feed') || item.includes('rss')) rss.push(item);
+      });
+      console.log(rss);
+      return rss;
+    })
+    .catch(function(err) {
+      console.log('err' + err);
     });
-    console.log(rss)
-  })
-  .catch(function(err){
-    console.log("err" + err)
-  });
-
-
-    
+}
