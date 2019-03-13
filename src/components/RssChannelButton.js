eace0ab0087ae802a2cdb4d8ee9e6b72ed997b46
Adam <amitrega01@gmail.com>
2019-03-13 21:16:11 +0100

    rss channels buttons

--- src/components/RssChannelButton.js
+++ src/components/RssChannelButton.js
@@ -4,25 +4,50 @@ import { connect } from 'react-redux';
 
 class RssChannelButton extends Component {
   render() {
-    return (
-      //TODO: wariant aktywny i nie aktywny
-      <TouchableOpacity
-        onPress={() => {
-          this.props.dispatch({
-            type: 'SET_ACTIVE',
-            id: this.props.id
-          });
-        }}
-      >
-        <Text>
-          {this.props.name}-{this.props.isActive ? 'T' : 'N'}
-        </Text>
-      </TouchableOpacity>
-    );
+    if (this.props.isActive) {
+      return (
+        //TODO: wariant aktywny i nie aktywny
+        <TouchableOpacity style={styles.active}>
+          <Text style={styles.activeText}>{this.props.name}</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <TouchableOpacity
+          style={styles.inActive}
+          onPress={() => {
+            this.props.dispatch({
+              type: 'SET_ACTIVE',
+              id: this.props.id
+            });
+          }}
+        >
+          <Text style={styles.inActiveText}>{this.props.name}</Text>
+        </TouchableOpacity>
+      );
+    }
   }
 }
 const mapStateToProps = state => ({
   channels: state.channels
 });
 export default connect(mapStateToProps)(RssChannelButton);
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  active: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderRadius: 16
+  },
+  activeText: {
+    color: 'rgba(0,0,0,0.75)',
+    fontWeight: 'bold'
+  },
+  inActive: {
+    paddingHorizontal: 16,
+    paddingVertical: 4
+  },
+  inActiveText: {
+    color: '#fff'
+  }
+});
