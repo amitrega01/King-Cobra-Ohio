7d6b691a3d4b9b38ebbedd073acac1f59994a8f1
maciejs11 <maciejs1112@gmail.com>
2019-03-14 09:55:42 +0100

    Dodano pola z propsami

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,56 +2,20 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 
-class Header extends Component {
-  render() {
-    return (
-      <View >
-        <Text style={{fontSize: 15, fontWeight:"bold"}}>{this.props.title}</Text>
-      </View>
-    )
-  }
-}
-
-class Link extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={{fontSize: 15}}>{this.props.name}</Text>
-      </View>
-    )
-  }
-}
-
-class Date extends Component {
-  render() {
-    return (
-      <View>
-        <Text style = {{fontSize: 10}}>{this.props.date}</Text>
-      </View>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={{fontSize: 12}}>{this.props.content}</Text>
-      </View>
-    )
-  }
-}
-
-
 export default class NewsItem extends Component {
   render() {
     return (
       <View>
-        <Header title = 'aaaaaa' />
-        <Link name = 'onet.pl'/>
-        <Date date = '09.09.2018'/>
-        <Content content = 'jdanskjdhakjsdhakjsdhkajs aksjdhjaak'/>
+        <Text style={{fontSize: 15, fontWeight:"bold"}}>{this.props.title}</Text>
+        <Text>{this.props.name} </Text>
+        <Text>{this.props.date} </Text>
+        <Text>{this.props.content}</Text>
       </View>
     )
   }
 }
+
+
+
+
+
