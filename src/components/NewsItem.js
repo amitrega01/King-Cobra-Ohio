29fe3c514a57a8df7f089ab181e12ec5e7822b5d
maciejs11 <maciejs1112@gmail.com>
2019-03-18 22:26:42 +0100

    Zmiana kolorów

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,23 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const styles =  StyleSheet.create({
         Header : {
-          fontSize: 30
+          fontSize: 30,
+          fontWeight: 'bold',
+          flexDirection: 'row'
+          
         },
 
-        container: {
-          
+        container: {         
           borderRadius: 10,
-          borderWidth: 0.5,
+          borderWidth: 1,
           borderColor: 'rgb(114, 114, 142)'
+          
+          
+          
+        },
+
+        Source: {
+          color: 'rgb(114, 114, 142)'
         }
       
 });
@@ -21,9 +30,9 @@ export default class NewsItem extends Component {
   render() {
     return (
       <View style = {styles.container}>
-        <Text style={styles.Header}>{this.props.title}</Text>
-        <Text>{this.props.name} </Text>
-        <Text>{this.props.date} </Text>
+        <Text style={styles.Header}>{this.props.title}</Text> 
+        <Text style={{flexDirection: 'row'}}>{this.props.date} </Text>
+        <Text style = {styles.Source}>{this.props.name} </Text>       
         <Text>{this.props.content}</Text>
       </View>
     )
