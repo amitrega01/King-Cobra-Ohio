361ee5fbce7d472b5e4481e7cd47fc2cd5a364dd
Adam <amitrega01@gmail.com>
2019-03-24 15:34:35 +0100

    fetching rss channels async to redux

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,88 +1,95 @@
 import React, { Component } from 'react';
-import { Text, AsyncStorage, View } from 'react-native';
+import { FlatList, Text, AsyncStorage, View } from 'react-native';
 import Strings from '../consts/Strings';
 import Styles from '../consts/Styles';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import rssFetch from '../utils/rssFetch';
 import NewsItem from '../components/NewsItem';
+import { NewsList } from '../containers/NewsList';
 
 const cheerio = require('cheerio-without-node-native');
+
+let list = <Text>Ladowanie</Text>;
+
 class HomeScreen extends Component {
   static navigationOptions = {
     header: null
   };
   constructor(props) {
     super(props);
+    this.state = {
+      loaded: false,
+      news: []
+    };
   }
 
-  async componentDidMount() {
+  async componentWillMount() {
     console.log('FIRST RUN');
-    var localStateJSON = await AsyncStorage.getItem('STATE1');
-    var localState = JSON.parse(localStateJSON);
-    console.log(localState);
-    if (localState == null) {
-      console.log('NIE MA W PAMIECI KANALOW');
-      var rss = await rssFetch(Strings.mainUrl);
-      for (let index = 0; index < rss.length; index++) {
-        rss[index] = {
-          id: index,
-          name: 'KANAL' + (index + 1),
-          url: rss[index],
-          isActive: index == 0 ? true : false
-        };
+    await AsyncStorage.getItem('STATE1').then(async localStateJSON => {
+      var localState = JSON.parse(localStateJSON);
+      console.log(localState);
+      if (localState == null) {
+        console.log('NIE MA W PAMIECI KANALOW');
+        await rssFetch(Strings.mainUrl).then(rss => {
+          var active = rss.find(item => item.isActive == true).news;
+          console.log('ACTIVE');
+          console.table(active);
+          this.props.dispatch({
+            type: 'UPDATE',
+            url: Strings.mainUrl,
+            rss: rss,
+            active: active
+          });
+          list = <NewsList news={active} />;
+          this.setState({
+            loaded: true
+          });
+        });
+      } else {
+        console.log('SA W PAMIECI');
+        this.props.dispatch({
+          type: 'GET_STATE_FROM_STORAGE',
+          localState: localState
+        });
+        list = (
+          <NewsList
+            news={this.props.channels.find(x => x.isActive == true).news}
+          />
+        );
+        this.setState({
+          loaded: true
+        });
       }
-      this.props.dispatch({ type: 'UPDATE', url: Strings.mainUrl, rss: rss });
-    } else {
-      console.log('SA W PAMIECI ');
-      this.props.dispatch({
-        type: 'GET_STATE_FROM_STORAGE',
-        localState: localState
-      });
-    }
+    });
   }
 
   firstRun = () => {};
   render() {
     const { navigate } = this.props.navigation;
+
     return (
       <View style={Styles.mainContainer}>
         <Header
+          callback={id => {
+            console.log('CALBBACk' + id);
+            let active = this.props.channels.find(item => item.id == id).news;
+            list = <NewsList news={active} />;
+            console.log(active);
+          }}
           channels={this.props.channels}
           onPress={() => navigate('Settings')}
         />
-        <FlatList
-          //TODO: data ma byc lista ktora jest aktywana
-          data={[
-            {
-              title: 'TYTUL',
-              date: 'data',
-              source: 'zrodlo',
-              content: 'LOREM IPSUm'
-            },
-            {
-              title: 'TYTUL',
-              date: 'data',
-              source: 'zrodlo',
-              content: 'LOREM IPSUm'
-            }
-          ]}
-          renderItem={item => (
-            <NewsItem
-              title={item.title}
-              date={item.date}
-              name={item.source}
-              content={item.content}
-            />
-          )}
-        />
+        <Text>{this.state.active}</Text>
+        {list}
       </View>
     );
   }
 }
 const mapStateToProps = state => ({
   url: state.url,
-  channels: state.channels
+  channels: state.channels,
+  active: state.active
 });
 
 export default connect(mapStateToProps)(HomeScreen);
