43769cdcb4fbc6d26341538b96aaaa43321062f3
maciejs11 <maciejs1112@gmail.com>
2019-03-26 22:12:46 +0100

    Usunięcie linku

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const styles =  StyleSheet.create({
         Header : {
-          fontSize: 30,
+          fontSize: 25,
           fontWeight: 'bold',
           flexDirection: 'row',
           justifyContent: 'space-between'
@@ -16,29 +16,21 @@ const styles =  StyleSheet.create({
           borderRadius: 10,
           borderWidth: 1,
           borderColor: 'rgb(114, 114, 142)',
-          padding: 10,
-             
-          
+          padding: 10,                     
         },
-
-        Source: {
-          color: 'rgb(114, 114, 142)'
-        },
+        
 
         Data: {
           color: 'rgb(114, 114, 142)',
           alignItems: 'stretch'
         },
         
-        Shadow: {
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 3,
-          },
-          shadowOpacity: 0.27,
-          shadowRadius: 4.65,
-          elevation: 4,
+        Shadow: {          
+          elevation: 6,
+        },
+
+        Background: {
+          backgroundColor: "#ffffff"
         }
       
         
@@ -49,12 +41,12 @@ export default class NewsItem extends Component {
   render() {
     
     return (
-      <View style = {[styles.container, styles.Shadow]}>
+      <View style = {[styles.container, styles.Shadow, styles.Background]}>
         <View style = {{flexDirection: 'row', justifyContent: 'space-between'}}>
           <Text style={styles.Header}>{this.props.title}</Text> 
           <Text style={styles.Data}>{this.props.date}</Text>
         </View>
-        <Text style = {styles.Source}>{this.props.name} </Text>       
+             
         <Text>{this.props.content}</Text>
       </View>
     )
