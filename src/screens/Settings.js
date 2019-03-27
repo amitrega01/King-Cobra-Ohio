2631d006aa1c0fd9f47fa850cb7f1a58243d8dbd
Wojtek <michalikwojtek19@gmail.com>
2019-03-27 13:34:15 +0100

    FirstLook

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Button, TextInput, Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 import Styles from '../consts/Styles';
 export default class Settings extends Component {
   static navigationOptions = {
@@ -7,8 +7,28 @@ export default class Settings extends Component {
   };
   render() {
     return (
-      <View style={Styles.mainContainer}>
-        <Text>Placeholder</Text>
+      <View style={{paddingHorizontal: 80, 
+        paddingVertical: 300}}>
+        <View 
+        style={{
+        
+        flexDirection: 'row'}}>
+          
+          <Text style={{flex: 1,}}>
+          Aktualizuj co:
+          </Text>
+          <TextInput
+            style={{flex: 1}}
+            maxLength = {3}
+            placeholder='np. 1'
+            keyboardType = 'numeric'
+          />
+
+        </View>
+        <View style={{paddingTop: 5}}>
+          <Button title="Zapisz" onPress=""></Button>
+        </View>
+
       </View>
     );
   }
