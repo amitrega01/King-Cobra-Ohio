3a199fceef0fae5cc9e8b3662fc7b380c4936d4b
Adam <amitrega01@gmail.com>
2019-03-27 16:30:18 +0100

    nowy string

--- src/consts/Strings.js
+++ src/consts/Strings.js
@@ -1,8 +1,10 @@
 const appTitle = 'ATHNews';
 const mainUrl = 'https://www.rmf24.pl/kanaly/rss';
 const updateInterval = 720000;
+const settings = 'Ustawienia';
 export default {
   appTitle,
   mainUrl,
-  updateInterval
+  updateInterval,
+  settings
 };
